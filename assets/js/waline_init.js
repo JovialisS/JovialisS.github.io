9fda8bcd2a1afa3d47d2abc599969d9c084ac27c
Jovialis <jovialis@outlook.com>
2026-01-10 01:42:14 +0800

    fff

--- assets/js/waline_init.js
+++ assets/js/waline_init.js
@@ -5,6 +5,8 @@ init({
     serverURL: 'https://jovially.netlify.app/.netlify/functions/comment',
     comment: true,
     reaction: true,
+    search: false,
+    lang: 'zh-CN',
     texRenderer: (blockMode, tex) =>
     window.MathJax.startup.adaptor.outerHTML(
         window.MathJax.tex2svg(tex, {
