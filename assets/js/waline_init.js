3474a400e608a2c92a245349bbea34050987f663
Jovialis <jovialis@outlook.com>
2026-01-10 11:36:28 +0800

    waline

--- assets/js/waline_init.js
+++ assets/js/waline_init.js
@@ -1,5 +1,7 @@
 import { init } from 'https://unpkg.com/@waline/client@v2/dist/waline.mjs';
 
+
+
 init({
     el: '#waline',
     serverURL: 'https://jovially.netlify.app/.netlify/functions/comment',
@@ -7,6 +9,13 @@ init({
     reaction: true,
     search: false,
     lang: 'zh-CN',
+    emoji: [
+      '//unpkg.com/@waline/emojis@1.4.0/tieba',
+      '//unpkg.com/@waline/emojis@1.4.0/bilibili',
+      '//unpkg.com/@waline/emojis@1.4.0/weibo',
+      '//unpkg.com/@waline/emojis@1.4.0/qq',
+    //   '//unpkg.com/@waline/emojis@1.4.0/tw-emoji',
+    ],
     texRenderer: (blockMode, tex) =>
     window.MathJax.startup.adaptor.outerHTML(
         window.MathJax.tex2svg(tex, {
@@ -14,3 +23,4 @@ init({
         }),
     ),
 });
+
